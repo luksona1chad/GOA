b8eee6035154fb3f39add35c9628e9d16cff6211
luksona1chad <ldatiashvili48@gmail.com>
2024-07-20 16:26:05 +0200

    first commit

---
+++ level 0/homework/homeworc.py
@@ -0,0 +1,138 @@
+from turtle import *
+# we want paint a square 
+# width (7)
+forward (200)
+left (90)
+forward (200)
+left (90)
+forward (200)
+left (90)
+forward (200)
+
+# we want paint a door
+left (90)
+forward (90)
+left (90)
+forward (90)
+right (90)
+forward (40)
+right (90)
+forward (90)
+
+#we want paint a windows
+
+penup ()
+goto (20 , 180)
+pendown ()
+
+left (90)
+forward (50)
+right (90)
+forward (50)
+right (90)
+forward (50)
+right (90)
+forward (50)
+
+penup ()
+goto ( 180 , 180) 
+pendown ()
+left (90)
+forward (50)
+left (90)
+forward (50)
+left (90)
+forward (50)
+left (90)
+forward (50)
+
+# we want paint a roof
+penup ()
+goto ( 200 , 200 )
+pendown ()
+left (45)
+forward (150)
+left (94)
+forward (150)
+
+# we want paint a hause 
+# we want paint and square 
+
+penup ()
+goto ( 250 , 200)
+pendown ()
+
+left (41)
+forward (300)
+left (90)
+forward (300)
+left (90)
+forward (300)
+left (90)
+forward (300)
+
+# we want paint  a roof
+# we want paint and a square
+right (135)
+forward (215)
+right (90)
+forward (215)
+
+# we want paint a windows 
+# we want paint and a square
+penup ()
+goto ( 450 , 150 )
+pendown ()
+right (45)
+forward (50)
+left (90)
+forward (50)
+left (90)
+forward (50)
+left (90)
+forward (50)
+
+penup ()
+goto ( 350 , 150)
+pendown ()
+forward (50)
+left (90)
+forward (50)
+left (90)
+forward (50)
+left (90)
+forward (50)
+
+# we want paint a door 
+# we wand paint and a square 
+penup ()
+goto ( 550 , 150 )
+pendown ()
+right (180)
+forward (250)
+right (90)
+forward (100)
+right (90)
+forward (100)
+left (90)
+forward (50)
+left (90)
+forward (100)
+
+# we want paint a hause 
+# we want paint and a square 
+penup ()
+goto ( 3 , 397 )
+pendown ()
+
+
+
+
+
+
+
+
+
+
+exitonclick ()
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
---
+++ level 03/homeworck/homeworck.py
@@ -0,0 +1,27 @@
+text = " Goa "
+print( text * 1000 )
+
+
+
+
+
+
+
+
+
+
+
+monkey = 15
+monkey_banana = 5
+print(monkey - monkey_banana)
+
+
+
+
+
+
+
+
+yer = 2014
+today = 2024
+print(today - yer )
