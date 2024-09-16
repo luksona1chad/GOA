f9ebaf59e9b35638b3caa25a60ae0e144baa701a
luksona1chad <ldatiashvili48@gmail.com>
2024-09-16 18:56:09 +0200

    commit

---
+++ level 25/classworck 25.py
@@ -0,0 +1,6 @@
+my_favorite_film = {"tranger thinks","old","equilaizer 4","attaco dei ginìganti","worden"}
+print("film-{0}")
+print("film-{1}")
+print("film-{2}")
+print("film-{3}")
+print("film-{4}")
