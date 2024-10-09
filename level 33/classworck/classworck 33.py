a0410a1b5108eef2509038e4a21001b604e383f5
luksona1chad <ldatiashvili48@gmail.com>
2024-10-09 09:56:17 +0200

    commit

---
+++ level 33/classworck/classworck 33.py
@@ -0,0 +1,17 @@
+
+name = "ლუკა დათიაშვილი"
+words = f"გამარჯობა ჩემი სახელი {name}"
+splited_word = words.split()
+joined_word = " ".join(splited_word)
+print(joined_word)
+#2)
+ჩემი_რაღაცეები_რაც_მიყვარს = "ტითქმის ყველაფერი"
+splited_word = ჩემი_რაღაცეები_რაც_მიყვარს .split()
+print(splited_word)
+#3)
+ჩემი_რაღაცეები_რაც_მიყვარს = "თითქმის ყველაფერი მიყვარს"
+splited_word = ჩემი_რაღაცეები_რაც_მიყვარს.split()
+joined_sentence = " ".join(splited_word)
+print(joined_sentence)
+
+
