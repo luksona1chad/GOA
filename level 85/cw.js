9d8bff4e552bd4a427299f6a325bde1982586546
luksona1chad <ldatiashvili48@gmail.com>
2025-04-15 19:01:17 +0200

    commit

---
+++ level 85/cw.js
@@ -0,0 +1,18 @@
+let name = "Luka is LOMI"
+const age = 10
+console.log(name)
+console.log(age)
+
+function lomi() {
+    console.log("მე ვარ ლომი :)))")
+}
+lomi()
+
+const body = document.getElementById("body")
+console.log(body)
+
+function handleChangeBg() {
+    body.style.background = "red"
+    console.log(name)
+    console.log('მე ვარ პანდა')
+}
