1a955bc84f91c4278ab299ec9dc2e36c1a4d1aef
Luka <ldatiashvili48@gmail.com>
2025-11-20 23:29:21 +0400

    commit

---
+++ level 146/cw/src/App.jsx
@@ -0,0 +1,12 @@
+import Home from "./components/home.jsx"
+function App() {
+  return <>
+    <div>
+      <h1>My App.jsx file</h1>
+      <p>My App.jsx file</p>
+      <Home></Home>
+    </div>
+  </>
+}
+
+export default App
---
+++ level 146/cw/src/components/home.jsx
@@ -0,0 +1,8 @@
+function Home() {
+    return<>
+    <div>
+        <img src="/ASSETS/IMGES/image.png"/>
+    </div>
+    </>
+}
+export default Home
