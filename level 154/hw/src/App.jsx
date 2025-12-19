3482f019ac1e35e953b4f7517e9f49a6fbab26bb
Luka <ldatiashvili48@gmail.com>
2025-12-19 14:17:49 +0400

    commit

--- level 154/hw/src/App.jsx
+++ level 154/hw/src/App.jsx
@@ -1,31 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react'
 
-function App() {
-  const [user, setUser] = useState({
-    name: "Luka",
-    age: "11/12",
-    email: "ldatiashvili48@gmail.com",
-  });
-
-  function handleSetNewUser() {
-    setUser({
-      name: "LUKA",
-      age: "12",
-      email: "Ldatiashvili48@gmail.com",
-    });
-  }
-
+export default function App() {
   return (
-    <div>
-      <h1>{user.name}</h1>
-      <h1>{user.age}</h1>
-      <h1>{user.email}</h1>
-
-      <button onClick={handleSetNewUser}>
-        IF U CICK U ARE G.. NO I JOKE HEHEHE
-      </button>
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+
+let user = {
+  name: "Luka",
+  age: 11,
+  email: "ldatiashvili48@gmail.com"
+};
+
+console.log(
+  `User name is ${user.name}, age is ${user.age}, email is ${user.email}`
+);
